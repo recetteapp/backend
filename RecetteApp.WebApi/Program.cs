4863149cf29b92fcfa40f23300e6a8d4277fdc1a
Isaac <icorbrey@gmail.com>
2021-12-12 23:32:06 -0500

    chore: Create API project

---
+++ RecetteApp.WebApi/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+	app.UseDeveloperExceptionPage();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
